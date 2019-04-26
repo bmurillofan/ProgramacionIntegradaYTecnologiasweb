00352192ba1f4040907d44c4346ec19d49160e53
wezarkeVo <wezarkevo@gmail.com>
2019-04-26 03:42:48 -0500

    Final
    Terminado bb

--- controller/dashboard_controller.js
+++ controller/dashboard_controller.js
@@ -11,7 +11,7 @@ var app = angular.module("dashboard", []);
 
 /*------------------------------- Area Modulo Controller --------------------------------*/
 //.controller ('Nombre Controller', directiva en function($scope)) Inyectables
-app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $http) {
+app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $http, $filter) {
 
 
   /*--------------------------    Area de Declaracion     ------------------------------*/
@@ -23,9 +23,63 @@ app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $h
 
   $scope.ciudadanos = [];
 
+  $scope.state = {
+    home:true,
+    other: false,
+    idUser: ''
+  };
+
+  $scope.target = [];
+
   /*--------------------------            $gui            ------------------------------*/
   //*************************(  Ejecuciones de la pantalla )******************************
   $scope.$gui = {
+    resetAll: ()=>{
+      $scope.state.other = false;
+      $scope.state.home = true;
+      $scope.target = [];
+      $scope.score.puntos = 0;
+      $scope.$gui.consultaPuntos(sessionState);
+    },
+    consultarOther: id => {
+      $scope.state.other = true;
+      $scope.state.home = false;
+      $scope.target = [];
+      $scope.score.puntos = 0;
+
+      var dataService = $http({
+        method: 'GET',
+        url: 'http://localhost:8084/EnSusManos/v1/eventos/ciudadano',
+        params: {
+          id: Number(id)
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      dataService.then(function(response) {
+        angular.forEach(response.data, function(value) {
+          $scope.target.push({
+            fechaevento: $filter('date')(new Date(value.fechaevento),'shortDate') ,
+            stuacion: value.stuacion,
+            idciudadano: value.idciudadano.nombre + ' ' + value.idciudadano.apellido,
+            correo: value.idciudadano.correo,
+            descripcion: value.idcriterio.descripcion,
+            puntuacion: Number(value.idcriterio.puntuacion)
+          });
+        });
+        $scope.score.puntos = 0;
+        angular.forEach(response.data, function(value) {
+          $scope.score.puntos = $scope.score.puntos + Number(value.idcriterio.puntuacion);
+        });
+
+        $scope.score.name = $scope.target && $scope.target[0] && $scope.target[0].idciudadano ? $scope.target && $scope.target[0] && $scope.target[0].idciudadano : 'NO NAME';
+
+      }, function(response) {
+        console.log("Error 500");
+      });
+    },
     consultaPuntos: function(correo) {
 
       var dataService = $http({
@@ -42,9 +96,13 @@ app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $h
       dataService.then(function(response) {
         $scope.score.puntos = Number(response.data[0].puntajeTotal);
         $scope.score.name = response.data[0].nombre;
+        $scope.state.idUser = response.data[0].id;
+        $scope.$gui.consultaMe();
       }, function(response) {
         console.log("Error 500");
       });
+
+
     },
     consultaCiudadanos: function() {
 
@@ -66,7 +124,40 @@ app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $h
       }, function(response) {
         console.log("Error 500");
       });
-    }
+    },
+    consultaMe: function() {
+
+          var dataService = $http({
+            method: 'GET',
+            url: 'http://localhost:8084/EnSusManos/v1/eventos/ciudadano',
+            params: {
+              id: Number($scope.state.idUser)
+            },
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+
+          dataService.then(function(response) {
+            angular.forEach(response.data, function(value) {
+              $scope.target.push({
+                fechaevento: $filter('date')(new Date(value.fechaevento),'shortDate') ,
+                stuacion: value.stuacion,
+                idciudadano: value.idciudadano.nombre + ' ' + value.idciudadano.apellido,
+                correo: value.idciudadano.correo,
+                descripcion: value.idcriterio.descripcion,
+                puntuacion: Number(value.idcriterio.puntuacion)
+              });
+            });
+            $scope.score.puntos = 0;
+            angular.forEach(response.data, function(value) {
+              $scope.score.puntos = $scope.score.puntos + Number(value.idcriterio.puntuacion);
+            });
+
+          }, function(response) {
+            console.log("Error 500");
+          });
+        }
   };
   //**************************************************************************************
   /*--------------------------            $gui            ------------------------------*/
@@ -107,6 +198,7 @@ app.controller('dashboard_Controller', function($scope, $timeout, $rootScope, $h
   let sessionState = document.cookie.split(';').length != 0 && document.cookie.split(';')[1] && document.cookie.split(';')[1].includes('correo') && document.cookie.split(';')[1].split('=').length == 2 ? document.cookie.split(';')[1].split('=')[1] : '';
   $scope.$gui.consultaPuntos(sessionState);
   $scope.$gui.consultaCiudadanos();
+
   /*--------------------------            Arranque         ------------------------------*/
 
 })
